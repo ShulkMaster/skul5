f9cc9d4e0d3f92a1a5e28b81d5c01c41f5e7de7a
shulkmaster <00088116@uca.edu.sv>
2020-05-17 13:53:16 -0600

    fisrt commit

---
+++ src/main/java/com/example/skul5/Skul5Application.java
@@ -0,0 +1,13 @@
+package com.example.skul5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skul5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skul5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skul5/Skul5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skul5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skul5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
